c8c6ba8ea57d7a221eda60ff8b0c91f1935b77bc
Giulio Bracci <giulio.bracci@sparkfabrik.com>
2024-07-01 15:28:56 +0200

    (feat): prettier

--- js/cursor.js
+++ js/cursor.js
@@ -1,4 +1,4 @@
-import { DOM } from "./dom.js";
+import { DOM } from './dom.js';
 
 export class Cursor {
   constructor(highlightSelector) {
@@ -6,44 +6,44 @@ export class Cursor {
     this.setup(this.highlightSelector);
   }
 
-  setMouseHighlight = (e) => {
-    DOM("body").map((el) => el.style.setProperty("--cursor-scale", 2));
-    DOM("body").map((el) =>
+  setMouseHighlight = e => {
+    DOM('body').map(el => el.style.setProperty('--cursor-scale', 2));
+    DOM('body').map(el =>
       el.style.setProperty(
-        "--cursor-text",
-        `"${e.target.getAttribute("data-cursor-text") || ""}"`
+        '--cursor-text',
+        `"${e.target.getAttribute('data-cursor-text') || ''}"`
       )
     );
   };
 
   removeMouseHighlight = () => {
-    DOM("body").map((el) => el.style.setProperty("--cursor-scale", 1));
-    DOM("body").map((el) => el.style.setProperty("--cursor-text", ""));
+    DOM('body').map(el => el.style.setProperty('--cursor-scale', 1));
+    DOM('body').map(el => el.style.setProperty('--cursor-text', ''));
   };
 
-  storeMouseCoordinates = (e) => {
-    DOM("body").map((el) => el.style.setProperty("--x", e.clientX + "px"));
-    DOM("body").map((el) => el.style.setProperty("--y", e.clientY + "px"));
+  storeMouseCoordinates = e => {
+    DOM('body').map(el => el.style.setProperty('--x', e.clientX + 'px'));
+    DOM('body').map(el => el.style.setProperty('--y', e.clientY + 'px'));
   };
 
   refresh = () => {
     this.setup(this.highlightSelector);
   };
 
-  setup = (targetSelector) => {
+  setup = targetSelector => {
     const mouseOverHighlightTargets = document.querySelectorAll(targetSelector);
-    mouseOverHighlightTargets.forEach((target) => {
-      target.addEventListener("mouseover", this.setMouseHighlight);
-      target.addEventListener("mouseout", this.removeMouseHighlight);
+    mouseOverHighlightTargets.forEach(target => {
+      target.addEventListener('mouseover', this.setMouseHighlight);
+      target.addEventListener('mouseout', this.removeMouseHighlight);
     });
-    document.addEventListener("mousemove", this.storeMouseCoordinates);
+    document.addEventListener('mousemove', this.storeMouseCoordinates);
 
     return () => {
-      mouseOverHighlightTargets.forEach((target) => {
-        target.removeEventListener("mouseover", this.setMouseHighlight);
-        target.removeEventListener("mouseout", this.removeMouseHighlight);
+      mouseOverHighlightTargets.forEach(target => {
+        target.removeEventListener('mouseover', this.setMouseHighlight);
+        target.removeEventListener('mouseout', this.removeMouseHighlight);
       });
-      document.removeEventListener("mousemove", this.storeMouseCoordinates);
+      document.removeEventListener('mousemove', this.storeMouseCoordinates);
     };
   };
 }
